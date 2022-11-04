a5c5b09f1099a13251e9b9ba4bb5b38310ea9272
Jamesmed1999 <74335705+Jamesmed1999@users.noreply.github.com>
2022-11-04 14:59:41 -0400

    Added custom search!

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-fetch("https://www.googleapis.com/books/v1/volumes?q=Hamlet") //replace q with search query 
+/*fetch("https://www.googleapis.com/books/v1/volumes?q=Hamlet") //replace q with search query 
 .then((response) => {
     if(response.ok)
     {
@@ -15,22 +15,58 @@ fetch("https://www.googleapis.com/books/v1/volumes?q=Hamlet") //replace q with s
     getBook(data)
 })
 .catch((error) => console.error("FETCH ERROR:", error));
-
+*/
 function getBook(data)
 {
-    const name = data.items[0].volumeInfo.title;
-    const writer = data.items[0].volumeInfo.authors[0];
-    //const bookimg = data.items[0].volumeInfo.imageLinks.thumbnail;
     const div = document.getElementById("result");
+
+    if(div.hasChildNodes())
+    {
+        while (div.firstChild) {
+            div.removeChild(div.firstChild);
+        }
+    }
+
+    for(let i = 0; i < 5; i++)
+    {
+    const name = data.items[i].volumeInfo.title;
+    const writer = data.items[i].volumeInfo.authors[0];
+   
+
+
     const heading = document.createElement("h1");
     const heading2 = document.createElement("h2");
     const bookimg = document.createElement("img");
-    bookimg.src = data.items[0].volumeInfo.imageLinks.thumbnail;
+    bookimg.src = data.items[i].volumeInfo.imageLinks.thumbnail;
     heading.innerHTML = name;
     heading2.innerHTML = writer;
     div.appendChild(bookimg);
     div.appendChild(heading);
     div.appendChild(heading2);
+    }
     
+}
+
+function apiSearch(term)
+{
+    
+    fetch("https://www.googleapis.com/books/v1/volumes?q=" + term) //replace q with search query 
+    .then((response) => {
+        if(response.ok)
+        {
+            return response.json();
+        }
+        else{
+            throw new Error("Network Response error")
+        }
+    })
+    .then(data => {
+        console.log(data.items[3]);
+        console.log(data.items[3].volumeInfo.title);
+        console.log(data.items[3].volumeInfo.authors[3]);
+        getBook(data)
+        
+    })
+    .catch((error) => console.error("FETCH ERROR:", error));
     
 }
