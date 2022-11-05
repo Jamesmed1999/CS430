d066739c128118c90773ae61695ab7b4ea903894
Simon Christian <77630749+simonchrist@users.noreply.github.com>
2022-11-04 21:07:59 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -30,19 +30,44 @@ function getBook(data)
     for(let i = 0; i < 25; i++)
     {
     const name = data.items[i].volumeInfo.title;
-    const writer = data.items[i].volumeInfo.authors[0];
-   
+    const writer = data.items[i].volumeInfo.authors;
+    const publisher= data.items[i].volumeInfo.publisher;
+    const publishedDate=data.items[i].volumeInfo.publishedDate;
+    const description=data.items[i].volumeInfo.description;
+    const pageCount=data.items[i].volumeInfo.pageCount;
 
 
-    const heading = document.createElement("h1");
-    const heading2 = document.createElement("h2");
+    const heading = document.createElement("h2");
+    const heading2 = document.createElement("h3");
+    const heading3=document.createElement("h3");
+    const heading4=document.createElement("h3");
+    const heading5=document.createElement("h4");
+    const heading6=document.createElement("h3");
+
+
     const bookimg = document.createElement("img");
+
+
+
     bookimg.src = data.items[i].volumeInfo.imageLinks.thumbnail;
-    heading.innerHTML = name;
-    heading2.innerHTML = writer;
+    heading.innerHTML = "Title: "+name;
+    heading2.innerHTML = "Author: "+ writer;
+    heading3.innerHTML="Publisher: "+publisher;
+    heading4.innerHTML="Published Date: "+publishedDate;
+    heading6.innerHTML="Page Count: "+pageCount;
+    heading5.innerHTML="Description: "+description;
+
+
+
     div.appendChild(bookimg);
     div.appendChild(heading);
     div.appendChild(heading2);
+    div.appendChild(heading3);
+    div.appendChild(heading4);
+    div.appendChild(heading6);
+    div.appendChild(heading5);
+
+
     }
     
 }
